6d8ee383c89b9008165bea0f1ff28b5cdfda3cea
onitwin <onitwin@gmail.com>
2020-09-16 16:19:31 +0100

    added extra styling for responsive layout

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -4,22 +4,24 @@ import  "./Tour.scss"
 
 class Tour extends Component{
 
+
   render(){
+    const {city,img,name,info}=this.props.tour
     return(
         <article className="tour">
         <div className="img-container">
-        <img src="https://images.pexels.com/photos/1105766/pexels-photo-1105766.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="howdy"/>
+        <img src={img}/>
         <span className="close-button"><i className="fas fa-window-close"/></span>
         </div>
         <div className="tour-info">
-        <h3>City</h3>
-        <h4>Name</h4>
+        <h3>{city}</h3>
+        <h4>{name}</h4>
         <h5>info{" "}
         <span>
         <i className="fas fa-caret-square-down"/>
         </span>
         </h5>
-        <p>Lorem ipsum</p>
+        <p>{info}</p>
 
         </div>
 
