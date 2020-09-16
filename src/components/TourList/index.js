8ab214e02dc6690b12472a8ddaa7cf0e9b01b29b
onitwin <onitwin@gmail.com>
2020-09-16 20:05:46 +0100

    completed tutorial

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -8,13 +8,19 @@ class TourList extends Component{
     tours:tourData
   }
 
+  removeTour=id=>{
+    const {tours}=this.state
+    const sortedTours=tours.filter(tour =>tour.id !==id)
+    this.setState({tours:sortedTours})
+  }
+
   render(){
     const {tours}=this.state;
     return(
       <section className="tourlist">
       {tours.map((tour)=>{
         return(
-          <Tour key={tour.id} tour={tour}/>
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour}/>
         )
       })}
 
